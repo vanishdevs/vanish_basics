4e86d58647a70ccb08077996549e770edc3f2f37
vanishdevs <22zVanish@gmail.com>
2024-07-30 15:32:51 -0400

    add(shared): Add some basic functions such as `Debug` and `ShowNotification` to be utlized

---
+++ shared/functions.lua
@@ -0,0 +1,10 @@
+-- @param debugPrint (string, number, table, etc) The message to print.
+function Debug(debugPrint)
+    print('[DEBUG]: ' ..  debugPrint)
+end
+
+-- @param msg (string) The necessary message that will be displayed in the notification
+-- @param source (number) The necessary source if you are sending a notification from the server
+function ShowNotification(msg, source) 
+    return source and lib.notify(source, {description = msg, position = 'top-right', type = 'inform'}) or lib.notify({description = msg, position = 'top-right', type = 'inform'})
+end
